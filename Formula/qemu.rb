6964d97f57729e82c2fdb0a2a326146ce6a31602
Mikołaj Miotk <15999352+mikeeq@users.noreply.github.com>
2026-09-23 13:11:58 +0000

    Update 1.0.27 source tarball sha256 to a5a8c09694ac5118a6e9068e4fa27502a41ecefc4dc0a0dd6665d111e06cfde2

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -5,7 +5,7 @@ class Qemu < Formula
 
   version "1.0.27"
   url "https://github.com/mikeeq/homebrew-qemu-virgl-kosmickrisp/archive/refs/tags/v1.0.27.tar.gz"
-  sha256 "3696476c21ad644ac3f5d30d968096b596649f4970f4031ffa49f1c551eaf990"
+  sha256 "a5a8c09694ac5118a6e9068e4fa27502a41ecefc4dc0a0dd6665d111e06cfde2"
   head "https://gitlab.com/qemu-project/qemu.git", branch: "master"
 
   bottle do
